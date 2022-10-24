0e1199e17b612f2a0003092c3bfe72d4e84e3266
RDDcat <yukmaro@gmail.com>
2022-10-24 20:14:03 +0900

    [UPDATE] 스프링5에서 권장하는 방식의 패스워드 인코더로 변경

--- SpringSecurity/src/main/java/maro/study/SpringSecurity/SpringSecurityApplication.java
+++ SpringSecurity/src/main/java/maro/study/SpringSecurity/SpringSecurityApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,8 +13,9 @@ public class SpringSecurityApplication {
 
 	@Bean
 	public PasswordEncoder passwordEncoder(){
-		// 스프링 시큐리티 5이전버전 인코더
-		return NoOpPasswordEncoder.getInstance();
+		// 스프링 시큐리티 5가 권장하는 인코더
+		// 다양한 패스워드 인코딩 방식을 지원한다.
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 	public static void main(String[] args) {
