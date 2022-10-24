0e0d5ac0637cd763f47f189c5cf195e9b6ecab8d
RDDcat <yukmaro@gmail.com>
2022-10-24 20:07:41 +0900

    [UPDATE] PasswordEncoder 빈 등록 (스트링 5 이전버전에서 사용하는 스타일)

--- SpringSecurity/src/main/java/maro/study/SpringSecurity/SpringSecurityApplication.java
+++ SpringSecurity/src/main/java/maro/study/SpringSecurity/SpringSecurityApplication.java
@@ -3,9 +3,18 @@ package maro.study.SpringSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
 public class SpringSecurityApplication {
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		// 스프링 시큐리티 5이전버전 인코더
+		return NoOpPasswordEncoder.getInstance();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
